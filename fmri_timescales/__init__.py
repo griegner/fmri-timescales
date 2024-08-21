920be12e570f71638b0ea8e6f653161cc64bb177
Gabriel Riegner <gabriel.riegner@gmail.com>
2024-08-21 15:49:49 -0700

    update license, mv setup to pyproject.toml

---
+++ fmri_timescales/__init__.py
@@ -0,0 +1,3 @@
+"""fmri_timescales."""
+
+__version__ = "0.0.1"
--- setup.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-from setuptools import find_packages, setup
-
-requirements = Path("requirements.txt").read_text().splitlines()
-
-setup(name="fmri_timescales", packages=find_packages(), install_requires=requirements)
