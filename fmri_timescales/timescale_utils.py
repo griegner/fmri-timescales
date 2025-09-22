938362085b26b61b8611ea768979b2b0fda4fb2a
Gabriel Riegner <gabriel.riegner@gmail.com>
2025-09-21 22:37:12 -0700

    change X_sfreq argument to lag_interval

--- fmri_timescales/timescale_utils.py
+++ fmri_timescales/timescale_utils.py
@@ -19,11 +19,11 @@ def newey_west_omega(u: np.ndarray, n_lags: Optional[int] = None) -> float:
     return omega
 
 
-def _phi_to_tau(phis: np.ndarray, se_phis: np.ndarray) -> tuple:
+def _phi_to_tau(phis: np.ndarray, se_phis: np.ndarray, lag: int = 1) -> tuple:
     """phi to tau (timescale), and apply delta method to std err"""
     phis_abs = np.abs(phis)  # tau undefined for negative phi
-    taus = -1.0 / np.log(phis_abs)
-    se_taus = (1.0 / (phis_abs * np.log(phis_abs) ** 2)) * se_phis
+    taus = -lag / np.log(phis_abs)
+    se_taus = (lag / (phis_abs * np.log(phis_abs) ** 2)) * se_phis
     return taus, se_taus
 
 
@@ -36,8 +36,8 @@ class LLS(BaseEstimator):
         The variance estimator to use. Options are "newey-west" or "non-robust", by default "newey-west"
     var_n_lags : int, optional
         The lag truncation number for the bartlett kernel, by default None
-    X_sfreq : float, optional
-        The sampling frequency in Hz, by default 1
+    lag_interval : int, optional
+        Separation in sampling intervals at which to compute lagged autocorrelation, by default 1
     copy_X : bool, optional
         If True X will be copied, else it may be overwritten, by default False
     n_jobs : int, optional
@@ -65,13 +65,13 @@ class LLS(BaseEstimator):
         self,
         var_estimator: str = "newey-west",
         var_n_lags: Optional[int] = None,
-        X_sfreq: Optional[float] = 1.0,
+        lag_interval: int = 1,
         copy_X: bool = False,
         n_jobs: Optional[int] = None,
     ) -> None:
         self.var_estimator = var_estimator
         self.var_n_lags = var_n_lags
-        self.X_sfreq = X_sfreq
+        self.lag_interval = lag_interval
         self.copy_X = copy_X
         self.n_jobs = n_jobs
 
@@ -79,7 +79,7 @@ class LLS(BaseEstimator):
     def _fit_lls(self, x: np.ndarray) -> tuple:
         """fit model to a single timeseries x in X"""
         T = len(x) - 1
-        lag = int(self.X_sfreq)
+        lag = self.lag_interval
         # x_t = X[1:], x_{t-1} = x[:-1] (Hz=1)
         phi_ = np.sum(x[lag:] * x[:-lag]) / np.sum(x[:-lag] ** 2)
 
@@ -129,7 +129,7 @@ class LLS(BaseEstimator):
         with Parallel(n_jobs=self.n_jobs) as parallel:
             lls_fits = parallel(self._fit_lls(X[:, idx]) for idx in range(X.shape[1]))
             phis_, se_phis_ = map(np.array, zip(*lls_fits))
-            taus_, se_taus_ = _phi_to_tau(phis_, se_phis_)
+            taus_, se_taus_ = _phi_to_tau(phis_, se_phis_, self.lag_interval)
 
         self.estimates_ = {"phi": phis_, "se(phi)": se_phis_, "tau": taus_, "se(tau)": se_taus_}
         return self
@@ -151,8 +151,8 @@ class NLS(BaseEstimator):
         The lag truncation number for the autocorrelation function, by default None
     X_domain : str, optional
         The domain of X. Options are "time" or "autocorrelation", be default "time"
-    X_sfreq : float, optional
-        The sampling frequency in Hz, by default 1
+    lag_interval : int, optional
+        Separation in sampling intervals at which to compute lagged autocorrelation, by default 1
     copy_X : bool, optional
         If True X will be copied, else it may be overwritten, by default False
     n_jobs : _type_, optional
@@ -181,7 +181,7 @@ class NLS(BaseEstimator):
         var_n_lags: Optional[int] = None,
         acf_n_lags: Optional[int] = None,
         X_domain: str = "time",
-        X_sfreq: Optional[float] = 1.0,
+        lag_interval: int = 1,
         copy_X: bool = False,
         n_jobs: Optional[int] = None,
     ) -> None:
@@ -190,7 +190,7 @@ class NLS(BaseEstimator):
         self.var_n_lags = var_n_lags
         self.acf_n_lags = acf_n_lags
         self.X_domain = X_domain
-        self.X_sfreq = X_sfreq
+        self.lag_interval = lag_interval
         self.copy_X = copy_X
         self.n_jobs = n_jobs
 
@@ -209,7 +209,8 @@ class NLS(BaseEstimator):
             raise ValueError("X_domain must be 'time' or 'autocorrelation'")
 
         # define the regression function (m), and its linearized regressor (dm_dphi)
-        ks = np.linspace(0, K - 1, K) * (1.0 / self.X_sfreq)
+        lag = self.lag_interval
+        ks = np.linspace(0, K - 1, K) * (1.0 / lag)
         m = lambda ks, phi: phi**ks
         dm_dphi = lambda ks, phi: ks * phi ** (ks - 1)
         jac = lambda ks, phi: dm_dphi(ks, phi + 1e-10).reshape(-1, 1)
@@ -217,8 +218,6 @@ class NLS(BaseEstimator):
         # phi estimator
         phi_, _ = curve_fit(f=m, xdata=ks, ydata=x_acf, p0=1e-2, bounds=(-1, +1), ftol=1e-6, jac=jac)
         phi_ = phi_.squeeze()
-
-        lag = int(self.X_sfreq + 1)
 
         # variance estimators
         def non_robust_time():
@@ -290,7 +289,7 @@ class NLS(BaseEstimator):
         with Parallel(n_jobs=self.n_jobs) as parallel:
             nls_fits = parallel(self._fit_nls(X[:, idx]) for idx in range(X.shape[1]))
             phis_, se_phis_ = map(np.array, zip(*nls_fits))
-            taus_, se_taus_ = _phi_to_tau(phis_, se_phis_)
+            taus_, se_taus_ = _phi_to_tau(phis_, se_phis_, self.lag_interval)
 
         self.estimates_ = {"phi": phis_, "se(phi)": se_phis_, "tau": taus_, "se(tau)": se_taus_}
         return self
